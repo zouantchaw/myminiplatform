ba8a2d540f538c8ed901f885785cfc41c70d2959
Wielfried Zouantcha <zouantchaw74@gmail.com>
2023-09-04 11:12:05 -0400

    add auth0(google) auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import { getServerSession, type NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import Auth0Provider from "next-auth/providers/auth0";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
@@ -20,6 +21,11 @@ export const authOptions: NextAuthOptions = {
         };
       },
     }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER
+    })
   ],
   pages: {
     signIn: `/login`,
