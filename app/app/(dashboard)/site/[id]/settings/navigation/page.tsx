4fb2a34cc7b76f67ffa1b7d3d12edc52aba17c85
Wielfried Zouantcha <zouantchaw74@gmail.com>
2023-09-12 07:26:41 -0400

    feat: Refactor SiteSettingsNavigation for enhanced UI/UX with Accordions and TypeScript integration

--- app/app/(dashboard)/site/[id]/settings/navigation/page.tsx
+++ app/app/(dashboard)/site/[id]/settings/navigation/page.tsx
@@ -1,37 +1,77 @@
-import { Accordion, AccordionHeader, AccordionBody } from "@tremor/react";
-import Form from "@/components/form";
-import { updateSite } from "@/lib/actions";
+'use client';
+import React, { useState, ChangeEvent } from "react";
+import {
+  TabGroup,
+  TabList,
+  Tab,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@tremor/react";
 
+interface NavItem {
+  name: string;
+  link: string;
+  editable: boolean;
+}
 
 export default function SiteSettingsNavigation() {
-  const dummyData = [
-    { name: "Home", link: "/" },
-    { name: "About", link: "/about" },
-    { name: "Contact", link: "/contact" },
-    { name: "Blog", link: "/blog" },
-  ];
+  const [navItems, setNavItems] = useState<NavItem[]>([
+    { name: "Home", link: "/", editable: false },
+    { name: "About", link: "/about", editable: true },
+    { name: "Contact", link: "/contact", editable: true },
+    { name: "Blog", link: "/blog", editable: true },
+  ]);
+
+  const handleEdit = (index: number, field: "name" | "link", value: string) => {
+    const updatedItems = [...navItems];
+    updatedItems[index][field] = value;
+    setNavItems(updatedItems);
+  };
 
   return (
-    <div className="flex flex-col space-y-6">
-      {dummyData.map((item, index) => (
-        <Accordion key={index}>
-          <AccordionHeader>{item.name}</AccordionHeader>
-          <AccordionBody>
-            <Form
-              title="Link"
-              description="The link for this navigation item."
-              helpText="Please use a valid URL."
-              inputAttrs={{
-                name: "link",
-                type: "text",
-                defaultValue: item.link,
-                placeholder: "/link",
-              }}
-              handleSubmit={updateSite}
-            />
-          </AccordionBody>
-        </Accordion>
-      ))}
+    <div className="flex w-full h-full">
+      {/* Edit Section */}
+      <div className="w-1/2 p-4 space-y-6 overflow-y-auto">
+        <h2 className="text-xl mb-4">Edit Navigation</h2>
+        {navItems.map((item, index) => (
+          <Accordion key={index}>
+            <AccordionHeader>{item.name}</AccordionHeader>
+            <AccordionBody>
+              <div className="space-y-2">
+                <label>Name</label>
+                <input
+                  type="text"
+                  value={item.name}
+                  disabled={!item.editable}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => handleEdit(index, "name", e.target.value)}
+                  className="block w-full p-2 mt-1 border rounded-md"
+                />
+                <label>Link</label>
+                <input
+                  type="text"
+                  value={item.link}
+                  disabled={!item.editable}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => handleEdit(index, "link", e.target.value)}
+                  className="block w-full p-2 mt-1 border rounded-md"
+                />
+              </div>
+            </AccordionBody>
+          </Accordion>
+        ))}
+      </div>
+
+      {/* Preview Section */}
+      <div className="w-1/2 p-4 bg-gray-100 flex flex-col items-center justify-center h-[500px]">
+        <h2 className="text-xl mb-4">Navigation Preview</h2>
+        <TabGroup>
+          <TabList className="flex justify-center space-x-4">
+            {navItems.map((item, index) => (
+              <Tab key={index}>{item.name}</Tab>
+            ))}
+          </TabList>
+        </TabGroup>
+      </div>
     </div>
   );
 }
